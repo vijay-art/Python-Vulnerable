fbb4de89faf93e1e3131e1deda5b52554593ca2a
Vijay rathod <54510977+vijay-art@users.noreply.github.com>
2024-02-04 10:31:03 +0530

    Create prtest.py

---
+++ prtest.py
@@ -0,0 +1,10 @@
+import openai as ai
+import json
+import streamlit as st
+
+print("** Loading API Key")
+ai.api_key = "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"
+
+# st.title("")
+st.markdown("# Cognavi - Job Description Generator")
+st.sidebar.markdown("# Job Description Generator")
